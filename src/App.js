71570bf09c6ee376690e8adba19ce40e4adaeeb5
Winifred Lu <winnie.lu@comcast.net>
2022-01-11 14:45:25 -0500

    initial DOM setup underway

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    user: {},
+    input: 'Github Handle',
+  }
+  componentDidMount() {
+    axios.get('https://api.github.com/users/maidiance')
+      .then(resp => {
+        console.log(resp.data);
+        this.setState({
+          ...this.state,
+          user: resp.data,
+        })
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return(
+    <div>
+      <h1>Github Info</h1>
+      <form>
+        <input/>
+        <button>Search</button>
+      </form>
+      <div>
+        <img src={this.state}
+      </div>
+    </div>
+    );
   }
 }
 
